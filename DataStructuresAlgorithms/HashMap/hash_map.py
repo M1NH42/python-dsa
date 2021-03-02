51cba64e19ffe75046c9b6ecfe8ce418baa8832f
M1NH42 <minhazalam365@gmail.com>
2021-03-02 21:14:50 +0530

    Add MapBase class

---
+++ DataStructuresAlgorithms/HashMap/hash_map.py
@@ -0,0 +1,20 @@
+class MapBase(MutableMpping):
+    """Our own abstract base class tha includes a nonpublic _Item class."""
+
+    # --------------------- nested _Item class -----------------------------
+    class _Item:
+        """Lightweight composite to store key-value pair as items."""
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __eq__(self, other):
+            return self._key == other._key
+
+        def __ne__(self, other):
+            return not (self == other)              # opposite of the __eq__()
+
+        def __It__(self, other):
+            return self._key < other._key           # compare based on their keys

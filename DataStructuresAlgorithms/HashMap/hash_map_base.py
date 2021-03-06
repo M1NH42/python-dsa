d90aba46d0356b370c2d0bb894c700617e24d195
M1NH42 <minhazalam365@gmail.com>
2021-03-06 15:49:48 +0530

    Add hash map base

---
+++ DataStructuresAlgorithms/HashMap/hash_map_base.py
@@ -0,0 +1,66 @@
+import random
+from collections.abc import MutableMapping
+
+
+class MapBase(MutableMapping):
+    """Our own abstract base class tha includes a nonpublic _Item class."""
+
+    # --------------------- nested _Item class -----------------------------
+    class _Item:
+        """Lightweight composite to store key-value pair as items."""
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __eq__(self, other):
+            return self._key == other._key
+
+        def __ne__(self, other):
+            return not (self == other)              # opposite of the __eq__()
+
+        def __It__(self, other):
+            return self._key < other._key  # compare based on their keys
+
+
+class HashMapBase(MapBase):
+    """Astract base class for map using hash-table with MAD compression."""
+
+    def __init__(self, cap=11, p=109345121):
+        """Create an empty hash table map."""
+        self._table = cap * [None]
+        self._n = 0  # number of netries in the map
+        self._prime = p  # prime for MAD compression
+        # scale from 1 to p-1 for MAD
+        self._scale = 1 + random.randrange(p - 1)
+        self._shift = random.randrange(p)  # shift from 0 to p-1 for MAD
+
+    def _hash_function(self, k):
+        return (hash(k) * self._scale + self._shift) % self._prime % len(self._table)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, k):
+        j = self._hash_function(k)
+        return self._bucket_getitem(j, k)  # may raise KeyError
+
+    def __setitem__(self, k, v):
+        j = self._hash_function(k)
+        self._bucket_setitem(j, k, v)  # subroutine maintains self._n
+        if self._n > len(self._table) // 2:  # keep load factor <= 0.5
+            # number 2^x - 1 is often prime
+            self._resize(2 * len(self._table) - 1)
+
+    def __delitem__(self, k):
+        j = self._hash_function(k)
+        self._bucket_delitem(j, k)
+        self._n -= 1
+
+    def _resize(self, c):
+        old = list(self.items())
+        self._table = c * [None]
+        self._n = 0
+        for (k, v) in old:
+            self[k] = v
